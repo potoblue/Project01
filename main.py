b34c8b7ce04d4406af754f9a611328642aa488cf
eunsik712 <eun21eun71@gmail.com>
2025-11-20 14:03:59 +0900

    add import

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# import
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import folium
 
 total_df = pd.read_csv("station_total_utf8.csv")
 senior_df = pd.read_csv("senior_total_utf8.csv")
@@ -11,3 +16,4 @@ print(total_df.columns)
 print(senior_df.head())
 print(senior_df.columns)
 # ------------------
+
