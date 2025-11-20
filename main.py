cb8eb63d2a470835682458a357032111afb52452
eunsik712 <eun21eun71@gmail.com>
2025-11-20 16:29:09 +0900

    인코딩

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ total_df = pd.read_csv("station_total_utf8.csv")
 senior_df = pd.read_csv("senior_total_utf8.csv")
 senior_care_df = pd.read_csv("senior_care_utf8.csv")
 sme_df = pd.read_csv("sme.csv")
-
+park_df = pd.read_csv("TB_PTP_PRK_M.csv")
 
 # 역별 승하차인원           => total_df
 # 65세 노인 승하차인원      => senior_df
@@ -26,5 +26,6 @@ print(senior_care_df.head())
 print(senior_care_df.columns)
 print(sme_df.head())
 print(sme_df.columns)
+print(park_df.head())
 # ------------------
 
